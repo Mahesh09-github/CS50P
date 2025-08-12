281944ccd60309b3e00c8c0230ffeede12011944
Mahesh09-github <biradarmahesh412@gmail.com>
2025-08-12 06:15:25 +0530

    week0

--- week0/sample.py
+++
@@ -1,8 +0,0 @@
-def main():
-    User_input=input("String: ")
-    if User_input[-3] in ["a","e","i","o","u"]:
-        print("it is a vowel")
-    else:
-        print("it is not a vowel")
-
-main()
