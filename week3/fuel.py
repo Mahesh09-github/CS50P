9b932f50c2688ba7359d97edfd785a5d0acf4f30
Mahesh09-github <biradarmahesh412@gmail.com>
2025-08-29 16:16:03 +0530

    week3

--- week3/fuel.py
+++ week3/fuel.py
@@ -1,3 +1,13 @@
+'''Fuel gauges indicate, often with fractions, just how much fuel is in a tank.
+ For instance 1/4 indicates that a tank is 25% full, 1/2 indicates that a tank is 50% full,and 3/4 indicates that a tank is 75% full.
+
+ In a file called fuel.py, implement a program that prompts the user for a fraction, formatted as X/Y, wherein each of X and Y is a positive integer,
+   and then outputs, as a percentage rounded to the nearest integer, how much fuel is in the tank.
+   If, though, 1% or less remains, output E instead to indicate that the tank is essentially empty.
+   And if 99% or more remains, output F instead to indicate that the tank is essentially full.
+
+If, though, X or Y is not an integer, X is greater than Y, or Y is 0, instead prompt the user again. (It is not necessary for Y to be 4.) Be sure to catch any exceptions like ValueError or ZeroDivisionError.'''
+
 def main():
     while True:
         try:
@@ -9,10 +19,10 @@ def main():
                 value=(x/y)*100
                 value=round(value)
                 if value<=1:
-                    print("E")
+                    print("Empty!!")
                     break
                 elif 99<=value:
-                    print("F")
+                    print("Full!!")
                     break
                 else:
                     print(f"{value}%")
--- week3/grocery.py
+++ week3/grocery.py
@@ -1,3 +1,12 @@
+'''1.Suppose that you’re in the habit of making a list of items you need from the grocery store.'''
+'''2.n a file called grocery.py, implement a program that prompts the user for items, one per line, 
+until the user inputs control-d (which is a common way of ending one’s input to a program).
+ 3.Then output the user’s grocery list in all uppercase, sorted alphabetically by item, prefixing each line with the number of times the user inputted that item.
+ 4. No need to pluralize the items.
+5. Treat the user’s input case-insensitively.'''
+
+
+
 def main():
     grocery={}
     while True:
